6ef70201056ea7d2da331b1ad916e2d187e08f67
Your Name <you@example.com>
2026-03-05 02:10:15 +0530

    feat: add Post Detail page and make homepage cards clickable

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('create/', views.create_post, name='create_post'),
     path('post/<int:pk>/edit/', views.edit_post, name='edit_post'),
     path('register/', views.register_view, name='register'),
--- blog/views.py
+++ blog/views.py
@@ -12,6 +12,11 @@ def index(request):
     return render(request, 'blog/index.html', {'posts': posts})
 
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
 @login_required
 def create_post(request):
     if request.method == 'POST':
