3e294e750e19beab3675c6955883008b0488a3e6
Your Name <you@example.com>
2026-03-05 02:12:02 +0530

    fix: use VideoCloudinaryStorage for video field

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from cloudinary_storage.storage import VideoCloudinaryStorage
 
 
 class Post(models.Model):
@@ -13,7 +14,12 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     image = models.ImageField(upload_to='blog_images/', blank=True, null=True)
-    video = models.FileField(upload_to='blog_videos/', blank=True, null=True)
+    video = models.FileField(
+        upload_to='blog_videos/',
+        storage=VideoCloudinaryStorage(),
+        blank=True,
+        null=True
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
